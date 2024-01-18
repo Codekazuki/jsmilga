f38d9173ceb313ea71d0678807756f1c02a7160f
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-01-18 13:36:14 +0100

    testing

--- src/Test.js
+++ src/Test.js
@@ -1,7 +1,43 @@
 import React from "react";
 
 function Test() {
-  return <div>Test</div>;
+  return (
+    <div>
+      <Booklist />
+    </div>
+  );
 }
+const Booklist = () => {
+  return (
+    <div>
+      <Book />
+      <Book />
+      <Book />
+    </div>
+  );
+};
+function Book() {
+  return (
+    <>
+      <Name />
+      <Image />
+      <Author />
+    </>
+  );
+}
+const Image = () => {
+  return (
+    <img
+      src='https://images-na.ssl-images-amazon.com/images/G/01/error/38._TTD_.jpg'
+      alt='amazon'
+    />
+  );
+};
 
+function Author() {
+  return <h1>ayodele</h1>;
+}
+function Name() {
+  return <h2>becoming a world class techie</h2>;
+}
 export default Test;
