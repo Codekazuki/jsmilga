057efe6fde806d123f09279380f026daf0da024a
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-01-21 22:06:42 +0100

    start

--- src/Test.js
+++ src/Test.js
@@ -1,5 +1,9 @@
 import React from "react";
-import myImage from "./assets/Screenshot (38).png";
+// import myImage from "./assets/Screenshot (38).png";
+
+const name = "Babafemi";
+const middleName = "toyin";
+const photol = "./public/images/Screenshot (41).png";
 
 function Test() {
   return (
@@ -20,23 +24,26 @@ const Booklist = () => {
 function Book() {
   return (
     <div className='book'>
-      <Name />
+      {/* <Name />
       <Image />
       <Book2 />
-      <Author />
+      <Author /> */}
+      <img src={photol} alt={name} />
+      <h2>{name}</h2>
+      <h1>{middleName}</h1>
     </div>
   );
 }
-const Image = () => {
-  return <img className='pix' src={myImage} alt='amazon' />;
-};
-const Book2 = () => (
-  <img className='pix' src='./images/Screenshot (41).png' alt='book2' />
-);
-function Author() {
-  return <h1>ayodele</h1>;
-}
-function Name() {
-  return <h2>becoming a world class techie</h2>;
-}
+// const Image = () => {
+//   return <img className='pix' src={myImage} alt='amazon' />;
+// };
+// const Book2 = () => (
+//   <img className='pix' src='./images/Screenshot (41).png' alt='book2' />
+// );
+// function Author() {
+//   return <h1>ayodele</h1>;
+// }
+// function Name() {
+//   return <h2>becoming a world class techie</h2>;
+// }
 export default Test;
