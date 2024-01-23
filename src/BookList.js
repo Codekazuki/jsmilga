877e054e5392e891d8ac27aeb4bc83ba507c76d4
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-01-23 13:07:13 +0100

    classwork better solution

--- src/BookList.js
+++ src/BookList.js
@@ -3,16 +3,19 @@ import { books } from "./books";
 import { Book } from "./Book";
 const BookList = () => {
   return (
-    <div>
-      {books.map((book, index) => {
-        // const { author, title, ima2, id } = book;
-        return (
-          <h1 className='book'>
-            <Book {...book} key={book.id} number={index} />
-          </h1>
-        );
-      })}
-    </div>
+    <>
+      <h1>Amazon best seller</h1>
+      <div>
+        {books.map((book, index) => {
+          // const { author, title, ima2, id } = book;
+          return (
+            <h1 className='book'>
+              <Book {...book} key={book.id} number={index} />
+            </h1>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
