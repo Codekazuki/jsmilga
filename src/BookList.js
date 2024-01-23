2629c986e02ddf14025295e4f1c0f7ff1c1a99bc
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-01-23 06:02:32 +0100

    image import struggle

--- src/BookList.js
+++ src/BookList.js
@@ -1,33 +1,37 @@
 import React from "react";
 
-const firstBook = {
-  author: "Adebayo Faleti",
-  title: "Ogboju Ode",
-  imag: "./images/mancap.png",
-  ima2: "ogboju-ode.png",
-};
-
-const secondBook = {
-  author: "Chinau Achebe",
-  title: "Things fall apart",
-  imag: "./images/mancap.png",
-  ima2: "ogboju-ode.png",
-};
-const thirdBook = {
-  author: "g.o fagunwa",
-  title: "lalude",
-  imag: "./images/mancap.png",
-  ima2: "ogboju-ode.png",
-};
+const books = [
+  {
+    author: "Adebayo Faleti",
+    title: "Ogboju Ode",
+    imag: "./images/mancap.png",
+    ima2: "ogboju-ode.png",
+  },
+  {
+    author: "Chinau Achebe",
+    title: "Things fall apart",
+    imag: "./images/mancap.png",
+    ima2: "ogboju-ode.png",
+  },
+  {
+    author: "g.o fagunwa",
+    title: "lalude",
+    imag: "./images/mancap.png",
+    ima2: "ogboju-ode.png",
+  },
+];
 
 const BookList = () => {
   return (
     <div>
-      <Book
-        title={firstBook.title}
-        ima2={firstBook.ima2}
-        author={firstBook.author}
-      />
+      {books.map((book) => {
+        const { author, title, ima2 } = book;
+        return (
+          <h1>
+            <Book author={author} title={title} ima2={ima2} key={book.author} />
+          </h1>
+        );
+      })}
     </div>
   );
 };
